a03c9b56ee47db8d64da4450d9b14cd0699c6318
Tyler Luce <tjluce@mac.com>
2017-07-11 23:08:12 -0400

    Made a normalizing function (and helper functions for that) to ensure that when adding multiple noise frequencies together they won't go over the range of -1, 1

--- main.py
+++ main.py
@@ -2,16 +2,45 @@ import noise
 import pygame
 import random
 
+def getMaxMinOfMapArray(mapArray):
+    maxNum = 0.0
+    minNum = 0.0
+
+    for y in mapArray:
+        for x in y:
+            if x > maxNum:
+                maxNum = x
+            if x < minNum:
+                minNum = x
+
+    return [minNum, maxNum]
+
+def mapNumberToRange(num, startMin, startMax, endMin, endMax):
+    # print(num - startMin, startMax - num, endMax - endMin)
+    return (num - startMin) / (startMax - startMin) * (endMax - endMin) + endMin
+
+def normalizeMapArray(mapArray):
+    maxAndMin = getMaxMinOfMapArray(mapArray)
+    startMin = maxAndMin[0]
+    startMax = maxAndMin[1]
+    newMapArray = []
+    for y in mapArray:
+        newMapArray.append(list(map(lambda x: mapNumberToRange(x, startMin, startMax, -1.0, 1.0), y)))
+
+    # for y in range(len(mapArray)):
+    #     for x in range(len(mapArray[y])):
+    #         print(mapArray[y][x], newMapArray[y][x])
+
+    return newMapArray
+
+# Assumes noise numbers are on a range of -1.0 to 1.0
 def convertPerlinToColor(perlin):
-    # shiftedPerlin = perlin + 1 / 2.0
-    # print(perlin)
-    # floatColor = shiftedPerlin * 255
     floatColor = (perlin + 1) / 2.0 * 255
     return round(floatColor)
 
-sizeX = 150
-sizeY = 150
-squareSize = 5
+sizeX = 500
+sizeY = 500
+squareSize = 1
 mapArray = []
 
 # print(mapArray)
@@ -19,25 +48,29 @@ mapArray = []
 random.seed()
 
 # Generate perlin noise
-# rand = random.randint(0, sizeX)
-# nz = ((rand / sizeX) - (sizeX / 2.0))
 
 nz = random.random()
 
 octaves = 4
-freq = 4.0
+freq = 1.0
 
 for y in range(sizeY):
     row = []
     for x in range(sizeX):
         nx = x/sizeX - (sizeX / 2.0)
         ny = y/sizeY - (sizeY / 2.0)
-        randNoise = noise.snoise3(freq * nx, freq * ny, freq * nz, octaves)
-        # print(perlin)
+        randNoise = noise.snoise3(1 * nx, 1 * ny, 1 * nz)
+        randNoise += .5 * noise.snoise3(2 * nx, 2 * ny, 2 * nz)
+        randNoise += .25 * noise.snoise3(4 * nx, 2 * ny, 2 * nz)
+        # print(randNoise)
         row.append(randNoise)
     mapArray.append(row)
 
-print(mapArray)
+# print(mapArray)
+
+mapArray = normalizeMapArray(mapArray)
+
+# print(mapArray)
 
 
 # Set up pygame for display
