d5dbf69253538f81e7dfd33f1f12f715ef65f90d
Tyler Luce <tjluce@mac.com>
2017-12-29 22:04:46 -0500

    Parallelized the generateNoiseValue function

--- generation.py
+++ generation.py
@@ -1,5 +1,6 @@
 import random, noise
 import generation
+from multiprocessing import Pool, TimeoutError
 
 # Loops through 2D array and returns the min and max values within it
 def getMaxMinOf2DArray(array):
@@ -37,54 +38,50 @@ def stretchValue(num, power):
         num = num.real
     return num
 
+def generateNoiseValue(nz, sizeX, sizeY, x, y, redistribPower):
+    nx = x / sizeX - (sizeX / 2.0)
+    ny = y / sizeY - (sizeY / 2.0)
+    randNoise = noise.snoise3(1 * nx, 1 * ny, 1 * nz, 1)
+    randNoise += .5 * noise.snoise3(2 * nx, 2 * ny, 2 * nz, 2)
+    randNoise += .25 * noise.snoise3(4 * nx, 2 * ny, 2 * nz, 4)
+
+    # Redistribution
+    randNoise = generation.stretchValue(randNoise, redistribPower)
+
+    return randNoise
+
 def generateElevationMoisture(sizeX, sizeY):
     elevation = []
     moisture = []
     # Generate elevation using noise
     nz = random.random()
 
-    octaves = 4
-    freq = 1.0
+    octaves = 4 #TODO: Use this
+    freq = 1.0 #TODO: Use this
     redistribPower = .3
 
-    for y in range(sizeY):
-        row = []
-        for x in range(sizeX):
-            nx = x / sizeX - (sizeX / 2.0)
-            ny = y / sizeY - (sizeY / 2.0)
-            randNoise = noise.snoise3(1 * nx, 1 * ny, 1 * nz, 1)
-            randNoise += .5 * noise.snoise3(2 * nx, 2 * ny, 2 * nz, 2)
-            randNoise += .25 * noise.snoise3(4 * nx, 2 * ny, 2 * nz, 4)
+    with Pool(processes=4) as pool:
+        for y in range(sizeY):
+            elevation.append(pool.starmap(generateNoiseValue, [(lambda x: (nz, sizeX, sizeY, x, y, redistribPower))(x) for x in range(sizeX)]))
 
-            # Redistribution
-            randNoise = generation.stretchValue(randNoise, redistribPower)
-
-            row.append(randNoise)
-        elevation.append(row)
+    pool.close()
+    pool.join()
 
     elevation = generation.normalize2DArray(elevation, -1.0, 1.0)
 
     # Generate moisture using noise
     nz = random.random()
 
-    octaves = 4
-    freq = 1.0
+    octaves = 4 #TODO: Use this
+    freq = 1.0 #TODO: Use this
     redistribPower = .3
 
-    for y in range(sizeY):
-        row = []
-        for x in range(sizeX):
-            nx = x / sizeX - (sizeX / 2.0)
-            ny = y / sizeY - (sizeY / 2.0)
-            randNoise = noise.snoise3(1 * nx, 1 * ny, 1 * nz, 1)
-            randNoise += .5 * noise.snoise3(2 * nx, 2 * ny, 2 * nz, 2)
-            randNoise += .25 * noise.snoise3(4 * nx, 2 * ny, 2 * nz, 4)
+    with Pool(processes=4) as pool:
+        for y in range(sizeY):
+            moisture.append(pool.starmap(generateNoiseValue, [(lambda x: (nz, sizeX, sizeY, x, y, redistribPower))(x) for x in range(sizeX)]))
 
-            # Redistribution
-            randNoise = generation.stretchValue(randNoise, redistribPower)
-
-            row.append(randNoise)
-        moisture.append(row)
+    pool.close()
+    pool.join()
 
     moisture = generation.normalize2DArray(moisture, -1.0, 1.0)
 
